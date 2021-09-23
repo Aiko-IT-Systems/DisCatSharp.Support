4b8cf6b0740e02d28e225d7fd5d27a8c53a19251
Lala Sabathil <aiko@aiko-it-systems.eu>
2021-09-23 04:21:17 +0200

    Context menu is lit :3

--- Commands/Tasks/ConduitTasks.cs
+++ Commands/Tasks/ConduitTasks.cs
@@ -1,6 +1,9 @@
 ﻿using DisCatSharp.ApplicationCommands;
 using DisCatSharp.ApplicationCommands.Attributes;
 using DisCatSharp.Entities;
+using DisCatSharp.Enums;
+using DisCatSharp.Interactivity;
+using DisCatSharp.Interactivity.Extensions;
 using DisCatSharp.Support.Providers;
 
 using Org.BouncyCastle.Crypto;
@@ -31,7 +34,7 @@ namespace DisCatSharp.Support.Commands.Tasks
         /// <param name="description">The description.</param>
         /// <returns>The url of the added task.</returns>
         [SlashCommand("create", "Creates a task")]
-        public static async Task CreateTaskAsync(InteractionContext ctx, 
+        public static async Task CreateTaskAsync(InteractionContext ctx,
             [ChoiceProvider(typeof(ConduitTaskTypeProvider)), Option("type", "Type")] string type,
             [ChoiceProvider(typeof(ConduitTaskPriorityProvider)), Option("priority", "Priority")] string priority,
             [Option("title", "Title")] string title,
@@ -89,6 +92,103 @@ namespace DisCatSharp.Support.Commands.Tasks
                 task.Status = "resolved";
                 m.Edit(task);
                 await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent($"Task closed: {Formatter.Bold(task.Title)}\nhttps://bugs.aitsys.dev/T{task.Identifier}"));
+            }
+            catch (Exception ex)
+            {
+                await ctx.Channel.SendMessageAsync(new DiscordEmbedBuilder()
+                {
+                    Title = "Error",
+                    Description = $"Exception: {ex.Message}\n" +
+                    $"```\n" +
+                    $"{ex.StackTrace}\n" +
+                    $"```"
+                });
+            }
+        }
+    }
+
+    /// <summary>
+    /// The conduit tasks context menu.
+    /// </summary>
+    public class ConduitTasksContextMenu : ApplicationCommandsModule
+    {
+        /// <summary>
+        /// Creates a task for https://bugs.aitsys.dev.
+        /// </summary>
+        /// <param name="ctx">The interaction context.</param>
+        /// <param name="type">The type.</param>
+        /// <param name="priority">The priority.</param>
+        /// <param name="title">The title.</param>
+        /// <param name="description">The description.</param>
+        /// <returns>The url of the added task.</returns>
+        [ContextMenu(ApplicationCommandType.Message, "Create a task")]
+        public static async Task CreateTaskFromMessageAsync(ContextMenuContext ctx)
+        {
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder() { IsEphemeral = false, Content = "Generating Task..." });
+            try
+            {
+                var interactivity = ctx.Client.GetInteractivity();
+
+                var type_select = await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().AddComponents(new DiscordSelectComponent($"task-create-type-{ctx.TargetMessage.Id}", "Select the type of the task", ConduitTaskTypeSelectProvider.GetSelectOptions(), 1, 1, false)).WithContent("What's the type?"));
+
+                var type_request = await interactivity.WaitForSelectAsync(type_select, x => x.Id == $"task-create-type-{ctx.TargetMessage.Id}" && x.User.Id == ctx.User.Id, TimeSpan.FromSeconds(60));
+
+                if (type_request.TimedOut)
+                {
+                    await ctx.DeleteFollowupAsync(type_select.Id);
+                    await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Timed out. Task creation failed"));
+                    return;
+                }
+
+                await ctx.DeleteFollowupAsync(type_select.Id);
+
+                var type = type_request.Result.Values[0];
+
+                var prio_select = await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().AddComponents(new DiscordSelectComponent($"task-create-prio-{ctx.TargetMessage.Id}", "Select the priority of this task", ConduitTaskPrioritySelectProvider.GetSelectOptions(), 1, 1, false)).WithContent("What's the priority?"));
+
+                var prio_request = await interactivity.WaitForSelectAsync(prio_select, x => x.Id == $"task-create-prio-{ctx.TargetMessage.Id}" && x.User.Id == ctx.User.Id, TimeSpan.FromSeconds(60));
+
+                if (prio_request.TimedOut)
+                {
+                    await ctx.DeleteFollowupAsync(prio_select.Id);
+                    await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Timed out. Task creation failed"));
+                    return;
+                }
+
+                await ctx.DeleteFollowupAsync(prio_select.Id);
+
+                var priority = prio_request.Result.Values[0];
+
+                var title_response = await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent("What's the title of the task?"));
+
+                var title_request = await interactivity.WaitForMessageAsync(m => m.Author.Id == ctx.User.Id && m.ChannelId == ctx.Channel.Id, TimeSpan.FromSeconds(60));
+
+                if (title_request.TimedOut)
+                {
+                    await ctx.DeleteFollowupAsync(title_response.Id);
+                    await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Timed out. Task creation failed"));
+                    return;
+                }
+
+                await ctx.DeleteFollowupAsync(title_response.Id);
+
+                var title = title_request.Result.Content;
+
+                await title_request.Result.DeleteAsync($"Interaction [task-create-{ctx.TargetMessage.Id}]");
+
+                Maniphest m = new(Bot.ConduitClient);
+                ManiphestTask task = new()
+                {
+                    Title = $"{type} {title}",
+                    Description = ctx.TargetMessage.Content + $"\n\nTicket source: {ctx.TargetMessage.JumpLink.AbsoluteUri}",
+                    Priority = priority
+
+                };
+                m.Edit(task);
+                task.SetProjects(Bot.Config.ConduitConfig.Projects);
+                task.SetSubscribers(Bot.Config.ConduitConfig.Subscribers);
+                m.Edit(task);
+                await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent($"Task created: https://bugs.aitsys.dev/T{task.Identifier}"));
             }
             catch (Exception ex)
             {
--- Providers/ConduitProvider.cs
+++ Providers/ConduitProvider.cs
@@ -35,6 +35,28 @@ namespace DisCatSharp.Support.Providers
     }
 
     /// <summary>
+    /// The conduit task priority select choices.
+    /// </summary>
+    internal class ConduitTaskPrioritySelectProvider
+    {
+        /// <summary>
+        /// Provides the priority choices for selects.
+        /// </summary>
+        public static IEnumerable<DiscordSelectComponentOption> GetSelectOptions()
+        {
+            return new DiscordSelectComponentOption[]
+            {
+                    new DiscordSelectComponentOption("Unbreak now!", "unbreak", null, false),
+                    new DiscordSelectComponentOption("Triage", "triage", null, false),
+                    new DiscordSelectComponentOption("High", "high", null, false),
+                    new DiscordSelectComponentOption("Normal", "normal", null, true),
+                    new DiscordSelectComponentOption("Low", "low", null, false),
+                    new DiscordSelectComponentOption("Wishlist", "wish", null, false)
+            };
+        }
+    }
+
+    /// <summary>
     /// The conduit task type provider.
     /// </summary>
     internal class ConduitTaskTypeProvider : IChoiceProvider
@@ -52,6 +74,26 @@ namespace DisCatSharp.Support.Providers
                     new DiscordApplicationCommandOptionChoice("Feature", "[Feature]"),
                     new DiscordApplicationCommandOptionChoice("Docs", "[Docs]"),
                     new DiscordApplicationCommandOptionChoice("Research", "[Research]")
+            };
+        }
+    }
+
+    /// <summary>
+    /// The conduit task priority provider.
+    /// </summary>
+    internal class ConduitTaskTypeSelectProvider
+    {
+        /// <summary>
+        /// Provides the priority choices for selects.
+        /// </summary>
+        public static IEnumerable<DiscordSelectComponentOption> GetSelectOptions()
+        {
+            return new DiscordSelectComponentOption[]
+            {
+                    new DiscordSelectComponentOption("Bug", "[Bug]", null, false),
+                    new DiscordSelectComponentOption("Feature", "[Feature]", null, false),
+                    new DiscordSelectComponentOption("Docs", "[Docs]", null, false),
+                    new DiscordSelectComponentOption("Research", "[Research]", null, false)
             };
         }
     }
