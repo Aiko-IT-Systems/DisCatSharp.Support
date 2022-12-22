f7cf413aa67ef89dda9b14ec442b1f086d1cdf49
Lala Sabathil <aiko@aitsys.dev>
2022-12-22 18:35:30 +0100

    add publish script

--- publish_linux.sh
+++ publish_linux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dotnet publish -c Release -r linux-x64 --self-contained=true -p:PublishSingleFile=false -p:GenerateRuntimeConfigurationFiles=true -o /opt/discatsharp-support DisCatSharp.Support.csproj
+dotnet publish -c Release -r linux-x64 --self-contained=true -p:PublishSingleFile=true -p:GenerateRuntimeConfigurationFiles=true -o /opt/discatsharp-support DisCatSharp.Support.csproj
